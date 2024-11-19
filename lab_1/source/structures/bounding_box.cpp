3ae7596a0ac5062370bdc05eb041ec8060d3d03a
Bora Büyükbas <borabuyukbas@hotmail.com>
2024-11-19 19:56:14 +0100

    Changelog: 19.11.2024, 17:26 (Korrektur in source/structures/bounding_box.cpp für das Plotten mit --universe-generator 1)
    Co-authored-by: Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>

--- lab_1/source/structures/bounding_box.cpp
+++ lab_1/source/structures/bounding_box.cpp
@@ -16,21 +16,26 @@ double BoundingBox::get_diagonal(){
 void BoundingBox::plotting_sanity_check(){
     // if one direction has a size of 0, convert to a square
     double x_size = x_max - x_min;
+    x_size = x_size < 0 ? -x_size : x_size;
     double y_size = y_max - y_min;
+    y_size = y_size < 0 ? -y_size : y_size;
 
-    if(x_size == 0 && y_size != 0){
+    if(x_size < 1e-10 && y_size > 1e-10){
         // fix x size
         //std::cout << "BoundingBox.sanity_check: inflating bounding box in x direction." << std::endl;
         x_min = x_min - (y_size / 2);
         x_max = x_max + (y_size / 2);
+        // update x_size
+        x_size = x_max - x_min;
+        x_size = x_size < 0 ? -x_size : x_size;
     }
-    if(y_size == 0 && x_size != 0){
+    if(y_size < 1e-10 && x_size > 1e-10){
         // fix y size
         //std::cout << "BoundingBox.sanity_check: inflating bounding box in y direction." << std::endl;
         y_min = y_min - (x_size / 2);
         y_max = y_max + (x_size / 2);
     }
-    if(x_size == 0 && y_size == 0){
+    if(x_size < 1e-10 && y_size < 1e-10){
         throw std::invalid_argument("x and y size of bounding box are 0, thus can not be fixed by the sanity check.");
     }
 }
