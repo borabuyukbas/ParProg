ffbc7fe1e636158f3371bcc52b68e4a2b4a80c97
Bora Büyükbas <borabuyukbas@hotmail.com>
2025-01-01 02:32:56 +0100

    move authors to the correct file and revert visual studio cmakelists change

--- lab_2/source/authors_lab1.h
+++ lab_2/source/authors_lab1.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define AUTHORS_TWO_A "Philip Seitz"
-#define AUTHORS_TWO_B "Philip Seitz"
-#define AUTHORS_TWO_C "Philip Seitz"
-#define AUTHORS_TWO_D "Philip Seitz"
-#define AUTHORS_TWO_E "??"
-#define AUTHORS_TWO_F "??"
+#define AUTHORS_TWO_A "Lukas Rothenberger"
+#define AUTHORS_TWO_B ""
+#define AUTHORS_TWO_C ""
+#define AUTHORS_TWO_D ""
+#define AUTHORS_TWO_E ""
+#define AUTHORS_TWO_F ""
 
 #define AUTHORS_THREE_A ""
 #define AUTHORS_THREE_B ""
