b25d2463a7cfb436f5f27137018f2a4d501e8657
Bora Büyükbas <borabuyukbas@hotmail.com>
2025-01-05 00:31:51 +0100

    Reimplement the construct functions for Quadtree

--- lab_2/source/quadtree/quadtree.cpp
+++ lab_2/source/quadtree/quadtree.cpp
@@ -1,12 +1,31 @@
 #include "quadtree.h"
 
 #include "quadtreeNode.h"
+#include "structures/bounding_box.h"
+#include "structures/universe.h"
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
 #include <set>
 #include <algorithm>
 #include <stdexcept>
 #include <omp.h>
+#include <vector>
 
-#define CUTOFF 800
+#define CUTOFF 10
+
+std::vector<std::int32_t> get_indices(Universe& universe, BoundingBox bounding_box, std::vector<std::int32_t> body_indices) {
+    // create a vector that holds incides for current quadrant
+    std::vector<std::int32_t> indices;
+
+    for (std::int32_t indice : body_indices) {
+        if (bounding_box.contains(universe.positions[indice])) {
+            indices.push_back(indice);
+        }
+    }
+    
+    return indices;
+}
 
 Quadtree::Quadtree(Universe& universe, BoundingBox bounding_box, std::int8_t construct_mode){
     if (!(construct_mode == 0 || construct_mode == 1 || construct_mode == 2)) {
@@ -48,7 +67,6 @@ void Quadtree::calculate_center_of_mass(){
     root->calculate_node_center_of_mass();
 }
 
-
 std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices){
     // initialize a new vector that contains subquadrants
     std::vector<QuadtreeNode*> subquadrants;
@@ -58,34 +76,24 @@ std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox B
         BoundingBox quadrant = BB.get_quadrant(i); // quadrant bounding box
         
         // create a vector that holds incides for current quadrant
-        std::vector<std::int32_t> indices;
-        for (std::int32_t indice : body_indices) {
-            if (quadrant.contains(universe.positions[indice])) {
-                indices.push_back(indice);
-            }
-        }
+        std::vector<std::int32_t> indices = get_indices(universe, quadrant, body_indices);
 
         int size = indices.size();
-
-        // if there is any body, append this as a new node to the subquadrants
-        if (size > 0) {
+        // if there is only one body, set the body identifier with the mass values and stop the recursion
+        if (size == 1) {
             QuadtreeNode* node = new QuadtreeNode(quadrant);
-
-            // if there is only one body, set the body identifier with the mass values and stop the recursion
-            if (size == 1) {
-                node->body_identifier = indices[0];
-                node->cumulative_mass = universe.weights[indices[0]];
-                node->cumulative_mass_ready = true;
-                node->center_of_mass = universe.positions[indices[0]];
-                node->center_of_mass_ready = true;
-            }
-            // if there are more then one bodies, search for the new children and copy the values back to the node
-            else if (size > 1) {
-                node->children = construct(universe, quadrant, indices);
-            }
-
-            // append the node in the subquadrants list
-            subquadrants.push_back(node);
+            node->body_identifier = indices[0];
+            node->cumulative_mass = universe.weights[indices[0]];
+            node->cumulative_mass_ready = true;
+            node->center_of_mass = universe.positions[indices[0]];
+            node->center_of_mass_ready = true;
+            subquadrants.push_back(node); // append the node in the subquadrants list
+        }
+        // if there are more then one bodies, search for the new children and copy the values back to the node
+        else if (size > 1) {
+            QuadtreeNode* node = new QuadtreeNode(quadrant);
+            node->children = construct(universe, quadrant, indices);
+            subquadrants.push_back(node); // append the node in the subquadrants list
         }
     }
 
@@ -93,110 +101,100 @@ std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox B
 }
 
 std::vector<QuadtreeNode*> Quadtree::construct_task(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices){
+    // initialize a new vector that contains subquadrants
     std::vector<QuadtreeNode*> subquadrants;
 
     #pragma omp parallel
     {
         #pragma omp single
         {
+            // loop over all quadrants
             for (int i = 0; i <= 3; ++i) {
-                BoundingBox quadrant = BB.get_quadrant(i);  // Get the quadrant bounding box
-
-                // Create a vector to hold indices for the current quadrant
-                std::vector<std::int32_t> indices;
-                for (std::int32_t indice : body_indices) {
-                    if (quadrant.contains(universe.positions[indice])) {
-                        indices.push_back(indice);
-                    }
-                }
+                BoundingBox quadrant = BB.get_quadrant(i); // quadrant bounding box
+                
+                // create a vector that holds incides for current quadrant
+                std::vector<std::int32_t> indices = get_indices(universe, quadrant, body_indices);
 
                 int size = indices.size();
-                // If there are bodies in this quadrant, create a node
-                if (size > 0) {
+
+                // if there is only one body, set the body identifier with the mass values and stop the recursion
+                if (size == 1) {
                     QuadtreeNode* node = new QuadtreeNode(quadrant);
+                    node->body_identifier = indices[0];
+                    node->cumulative_mass = universe.weights[indices[0]];
+                    node->cumulative_mass_ready = true;
+                    node->center_of_mass = universe.positions[indices[0]];
+                    node->center_of_mass_ready = true;
 
-                    // If there is only one body, set the body identifier with the mass values
-                    if (size == 1) {
-                        node->body_identifier = indices[0];
-                        node->cumulative_mass = universe.weights[indices[0]];
-                        node->cumulative_mass_ready = true;
-                        node->center_of_mass = universe.positions[indices[0]];
-                        node->center_of_mass_ready = true;
-                    }
-                    // If there are more than one body, create tasks for the children
-                    else {
-                        #pragma omp task shared(node)
-                        {
-                            node->children = construct_task(universe, quadrant, indices);
-                        }
-                    }
-
-                    // Append the node to the shared vector (critical section to avoid race condition)
                     #pragma omp critical
+                    subquadrants.push_back(node); // append the node in the subquadrants list
+                }
+                // if there are more then one bodies, search for the new children and copy the values back to the node
+                else if (size > 1) {
+                    #pragma omp task
                     {
-                        subquadrants.push_back(node);
+                        QuadtreeNode* node = new QuadtreeNode(quadrant);
+                        node->children = construct_task(universe, quadrant, indices);
+
+                        #pragma omp critical
+                        subquadrants.push_back(node); // append the node in the subquadrants list
                     }
                 }
             }
         }
-
-        // Wait for all tasks to finish before returning the result
-        #pragma omp taskwait
     }
 
     return subquadrants;
 }
 
 std::vector<QuadtreeNode*> Quadtree::construct_task_with_cutoff(Universe& universe, BoundingBox& BB, std::vector<std::int32_t>& body_indices){
+    // initialize a new vector that contains subquadrants
     std::vector<QuadtreeNode*> subquadrants;
 
     #pragma omp parallel
     {
         #pragma omp single
         {
+            // loop over all quadrants
             for (int i = 0; i <= 3; ++i) {
-                BoundingBox quadrant = BB.get_quadrant(i);  // Get the quadrant bounding box
-
-                // Create a vector to hold indices for the current quadrant
-                std::vector<std::int32_t> indices;
-                for (std::int32_t indice : body_indices) {
-                    if (quadrant.contains(universe.positions[indice])) {
-                        indices.push_back(indice);
-                    }
-                }
+                BoundingBox quadrant = BB.get_quadrant(i); // quadrant bounding box
+                
+                // create a vector that holds incides for current quadrant
+                std::vector<std::int32_t> indices = get_indices(universe, quadrant, body_indices);
 
                 int size = indices.size();
-                // If there are bodies in this quadrant, create a node
-                if (size > 0) {
-                    QuadtreeNode* node = new QuadtreeNode(quadrant);
 
-                    // If there is only one body, set the body identifier with the mass values
-                    if (size == 1) {
-                        node->body_identifier = indices[0];
-                        node->cumulative_mass = universe.weights[indices[0]];
-                        node->cumulative_mass_ready = true;
-                        node->center_of_mass = universe.positions[indices[0]];
-                        node->center_of_mass_ready = true;
-                    }
-                    // If there are more than one body, create tasks for the children
-                    else {
-                        #pragma omp task shared(node) if (size > CUTOFF)
-                        {
+                // if there is only one body, set the body identifier with the mass values and stop the recursion
+                if (size == 1) {
+                    QuadtreeNode* node = new QuadtreeNode(quadrant);
+                    node->body_identifier = indices[0];
+                    node->cumulative_mass = universe.weights[indices[0]];
+                    node->cumulative_mass_ready = true;
+                    node->center_of_mass = universe.positions[indices[0]];
+                    node->center_of_mass_ready = true;
+
+                    #pragma omp critical
+                    subquadrants.push_back(node); // append the node in the subquadrants list
+                }
+                // if there are more then one bodies, search for the new children and copy the values back to the node
+                else if (size > 1) {
+                    #pragma omp task final (size <= CUTOFF)
+                    {
+                        QuadtreeNode* node = new QuadtreeNode(quadrant);
+
+                        if (size > CUTOFF) {
                             node->children = construct_task_with_cutoff(universe, quadrant, indices);
                         }
-                    }
+                        else {
+                            node->children = construct(universe, quadrant, indices);
+                        }
 
-                    // Append the node to the shared vector (critical section to avoid race condition)
-                    #pragma omp critical
-                    {
-                        subquadrants.push_back(node);
+                        #pragma omp critical
+                        subquadrants.push_back(node); // append the node in the subquadrants list
                     }
                 }
             }
         }
-
-        // Wait for all tasks to finish before returning the result
-        #pragma omp taskwait
     }
 
     return subquadrants;
